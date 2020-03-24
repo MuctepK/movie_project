a482ded3352eb90949ae45dc7526bfc7e8d0f07c
Kutman Mairambek uulu <muctep.k@gmail.com>
2020-03-24 22:42:01 +0600

    Закончил с главной страницей

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from webapp.models import Movie
+from webapp.models import Movie, Genre
 
 
 class IndexView(TemplateView):
@@ -9,6 +10,8 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['slider_movies'] = Movie.objects.all()[:7]
-        context['new_movies'] = Movie.objects.all()
+        context['slider_movies'] = Movie.objects.all()[:6]
+        context['movies'] = Movie.objects.all()
+        context['genres'] = Genre.objects.filter(movies__isnull=False).annotate(count=Count('movies')).order_by('-count')[:4]
+        print(context['genres'])
         return context
